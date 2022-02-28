a1e3cf4e5f24593926aaa5411c02a4a48ccaffed
1Chark1 <hueveo117@gmail.com>
2022-02-28 00:21:26 -0600

    se agrego un posible eliminar

--- prueba de arreglos/guardar datos.cpp
+++ prueba de arreglos/guardar datos.cpp
@@ -1,22 +1,26 @@
 #include <iostream>
 #include <cstdio>
+#include <conio.h>
 
 using namespace std;
 int main() {
 
 	string productos[100];
 	int opc = 0, i=0;
+	char vacio = ' ';
 
-	while (opc != 4) {
+	while (opc != 5) {
 		system("cls");
 		cout << "1 producto" << endl;
 		cout << "2 modificar" << endl;
 		cout << "3 ver" << endl;
-		cout << "4 salir" << endl;
+		cout << "4 eliminar" << endl;
+		cout << "5 salir" << endl;
 		cin >> opc;
 
 		string nombre;
 		string nuevo;
+		string eliminar;
 
 		switch (opc) {
 
@@ -42,19 +46,40 @@ int main() {
 
 			}
 
+			system("pause>nul");
+
 			break;
 
 		case 3:
 			for (int j = 0; j < i; j++) {
-				cout << "-> " << (j + 1) << productos[j] << endl;
+				cout << "-> " << (j + 1) << "-" << productos[j] << endl;
 
 			}
 
-			system("pause");
+			system("pause>nul");
 
 			break;
 
 		case 4:
+			cout << "Ingrese la letra que desea borrar: " << endl;
+			cin >> nombre;
+
+			for (int i = 0; i < nombre.length(); ++i) {
+				if (productos[i] == nombre) {
+					productos[i] = eliminar;
+				}
+				else {
+					nombre += productos[i];
+				}
+			}
+
+			cout << "se a eliminado: " << nombre << "\n";
+			
+			system("pause>nul");
+			break;
+			
+
+		case 5:
 			exit(1);
 			
 			break;
